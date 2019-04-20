925318a2b9b1f47e10a9ea604ff86a43a412cb33
Ian Matthew Davidson <idavidson@blue.cs.sonoma.edu>
2019-04-20 15:28:24 -0700

    removed the front end stuff, as it is more straightforward to have its own repo

--- advising/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Advisor from './Advisor';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Advisor />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
